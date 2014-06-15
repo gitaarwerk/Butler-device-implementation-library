4eedb69f2bfb0dc43b5aeacf3302fec66fb852e0
Jordy Houtman <j.houtman@coolblue.nl>
2014-06-15 16:58:38 +0200

    Moved the controller building its own controller factory class

--- Framework/Core/CoreFunctions.php
+++ Framework/Core/CoreFunctions.php
@@ -51,40 +51,4 @@ class CoreFunctions
             }
         }
     }
-
-    public static function controllerFactory($url)
-    {
-        if( \Framework\Defaults\Type\String::isDefault($url) === true)
-        {
-            \Framework\Defaults\Exceptions\Exception::Error('No url given');
-            return;
-        }
-
-        $urlArray = array();
-        $urlArray = explode("/", $url);
-
-        $controller = ucwords($urlArray[0]);
-        array_shift($urlArray);
-        $action = $urlArray[0];
-        array_shift($urlArray);
-        $queryString = $urlArray;
-
-        $controller = FRAMEWORK_CLASS_ROOT_NAME . FRAMEWORK_CLASS_SEPARATOR . FRAMEWORK_CONTROLLER_NAME . FRAMEWORK_CLASS_SEPARATOR . $controller;
-        $model = rtrim($controller, 's');
-
-        echo $controller;
-        echo '<hr>';
-        echo $model;
-
-        $dispatch = new $controller($model, $controller, $action);
-
-        if ((int)method_exists($controller, $action))
-        {
-            call_user_func_array(array($dispatch, $action), $queryString);
-        }
-        else
-        {
-            \Framework\Defaults\Exceptions\Exception::Notice("no controller found..");
-        }
-    }
 }
