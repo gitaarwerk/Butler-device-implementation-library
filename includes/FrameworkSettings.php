a892ac002a90589fdb8c1e175ce465056b01ea71
Jordy Houtman <j.houtman@coolblue.nl>
2014-06-15 15:39:43 +0200

    Removed existing framework

--- includes/FrameworkSettings.php
+++
@@ -1,9 +0,0 @@
-<?php
-// set some global static classes & framework constants
-
-define('FRAMEWORK_URL_PORT_SEPARATOR', ":");
-define('FRAMEWORK_URL_DOMAIN_SEPARATOR', ".");
-define('FRAMEWORK_URL_FOLDER_SEPARATOR', "/");
-define('FRAMEWORK_URL_PROTOCOL_SEPARATOR', "://");
-
-?>
--- includes/settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('FRAMEWORK_SHOW_ERRORS', true) // Show fraemwork errors (true or false)
-
-
-?>
