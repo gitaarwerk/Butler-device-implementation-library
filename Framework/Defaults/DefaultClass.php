139a3412aba00ff5a920444ffda529c4609d5fb7
Jordy Houtman <jhoutman@users.noreply.github.com>
2014-06-23 08:19:41 +0200

    Added final to magic methods implementation

--- Framework/Defaults/DefaultClass.php
+++ Framework/Defaults/DefaultClass.php
@@ -4,22 +4,22 @@ namespace Framework\Defaults;
 
 abstract class DefaultClass
 {
-    public static function __invoke()
+    	final public static function __invoke()
 	{
 		\Framework\Defaults\Exceptions\Exception::Error("You cannot use a class as a method...", 1, false);
 	}
 	
-	public function __toString()
+	final public function __toString()
 	{
 		return \Framework\Defaults\Exceptions\Exception::Error("You used a class as a value...", 0, false);
 	}
 	
-	public function __get($name)
+	final public function __get($name)
 	{
 		return \Framework\Defaults\Exceptions\Exception::Error("The variable: " . $name . " is not set...", 0, false);
 	}
 	
-	public function __set($name, $vars)
+	final public function __set($name, $vars)
 	{
 		return \Framework\Defaults\Exceptions\Exception::Error("You may not set the variable" . $name . "...", 0, false);
 	}
